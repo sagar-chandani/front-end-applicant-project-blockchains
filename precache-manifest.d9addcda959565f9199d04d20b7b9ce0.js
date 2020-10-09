3df1768104d2b1fc107ce169fd9b887e1a1f8874
sagar-chandani <spc1194@gmail.com>
2020-10-08 20:55:59 -0500

    Updates

---
+++ precache-manifest.d9addcda959565f9199d04d20b7b9ce0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7d48306f2d0ba32a776245d8cb4f323",
+    "url": "/front-end-applicant-project-blockchains/index.html"
+  },
+  {
+    "revision": "813f2bff716f3aa82254",
+    "url": "/front-end-applicant-project-blockchains/static/css/main.e1857894.chunk.css"
+  },
+  {
+    "revision": "e106f31f193fbb523f2e",
+    "url": "/front-end-applicant-project-blockchains/static/js/2.99e269f0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/front-end-applicant-project-blockchains/static/js/2.99e269f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "813f2bff716f3aa82254",
+    "url": "/front-end-applicant-project-blockchains/static/js/main.6659ef22.chunk.js"
+  },
+  {
+    "revision": "e74998ae795ace9071c7",
+    "url": "/front-end-applicant-project-blockchains/static/js/runtime-main.0081650e.js"
+  }
+]);
